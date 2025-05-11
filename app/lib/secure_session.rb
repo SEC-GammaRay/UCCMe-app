8b629f2a6110bf9a18b5d2f8e5a32cdff571e453
yitzuyao <bellayytrose@gmail.com>
2025-05-12 02:08:18 +0800

    add: app/lib

---
+++ app/lib/secure_session.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative 'secure_message'
+
+# Encrypt and Decrypt JSON encoded sessions
+class SecureSession
+
+  ## Class methods to create and retrieve cookie salt
+  SESSION_SECRET_BYTES = 64
+
+  # Generate secret for sessions
+  def self.generate_secret
+    SecureMessage.encoded_random_bytes(SESSION_SECRET_BYTES)
+  end
+
+  ## Instance methods to store and retrieve encrypted session data
+  def initialize(session)
+    @session = session
+  end
+
+  def set(key, value)
+    @session[key] = SecureMessage.encrypt(value).to_s
+  end
+
+  def get(key)
+    return nil unless @session && @session[key]
+
+    SecureMessage.new(@session[key]).decrypt
+  end
+
+  def delete(key)
+    @session.delete(key)
+  end
+end
