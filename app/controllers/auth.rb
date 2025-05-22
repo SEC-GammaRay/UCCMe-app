34d67492eef9313374a37adbb9c1423be0829e73
yuhsiangl <aalex910606@gmail.com>
2025-05-22 11:30:26 +0800

    feat: register

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -40,6 +40,26 @@ module UCCMe
           routing.redirect @login_route
         end
       end
+
+      @register_route = '/auth/register'
+      routing.is 'register' do
+        routing.get do
+          view :register
+        end
+
+        routing.post do
+          account_data = routing.params.transform_keys(&:to_sym)
+          CreateAccount.new(App.config).call(**account_data)
+
+          flash[:notice] = 'Please login with your new account information!'
+          routing.redirect @login_route
+        rescue StandardError => e
+          App.logger.error "ERROR CREATING ACCOUNT: #{e.inspect}"
+          App.logger.error e.backtrace
+          flash[:error] = 'Could not create account'
+          routing.redirect @register_route
+        end
+      end
     end
   end
 end
