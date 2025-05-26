ba86c6364b487b39afc48aa46dd1918b423af056
yuhsiangl <aalex910606@gmail.com>
2025-05-27 03:38:56 +0800

    front-end: change input style

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -16,6 +16,30 @@ module UCCMe
             routing.redirect '/auth/login'
           end
         end
+
+        # POST /account/<registration_token>
+        routing.post String do |registration_token|
+          raise 'Passwords do not match or empty' if
+            routing.params['password'].empty? ||
+            routing.params['password'] != routing.params['password_confirm']
+
+          new_account = SecureMessage.new(registration_token).decrypt
+          CreateAccount.new(App.config).call(
+            email: new_account['email'],
+            username: new_account['username'],
+            password: routing.params['password']
+          )
+          flash[:notice] = 'Account created! Please login'
+          routing.redirect '/auth/login'
+        rescue CreateAccount::InvalidAccount => e
+          flash[:error] = e.message
+          routing.redirect '/auth/register'
+        rescue StandardError => e
+          flash[:error] = e.message
+          routing.redirect(
+            "#{App.config.APP_URL}/auth/register/#{registration_token}"
+          )
+        end
       end
     end
   end
